206dfc47d866e2d98395165843732ae79df74b2a
1135975331 <dkswnstj021@gmail.com>
2024-02-28 08:21:11 +0900

    change the struct name from OrderStartTicks to OrderStartTimes

--- Utils/MiscellaneousConversionUtil.cs
+++ Utils/MiscellaneousConversionUtil.cs
@@ -8,8 +8,8 @@ public static class MiscellaneousConversionUtil
     public static byte GetOrderNum(int tick)
     {
         for(byte orderNum = 0; orderNum <= MaxOrderNum; orderNum++) {
-            var curOrderStartTick = OrderStartTicks[orderNum].StartTick;
-            var nextOrderStartTick = orderNum+1 <= MaxOrderNum ? OrderStartTicks[orderNum+1].StartTick : int.MaxValue;
+            var curOrderStartTick = OrderStartTimes[orderNum].StartTick;
+            var nextOrderStartTick = orderNum+1 <= MaxOrderNum ? OrderStartTimes[orderNum+1].StartTick : int.MaxValue;
 
             if(curOrderStartTick <= tick && tick < nextOrderStartTick)
                 return orderNum;
